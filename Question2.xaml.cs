42ff2a8826b6b37dd1a4998a4326641e0b5a0c73
Johnny <Johnny@DESKTOP-31T0HM6>
2020-05-06 18:39:05 -0400

    first commit

---
+++ Question2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSharp.Net_Quiz3
+{
+    /// <summary>
+    /// Interaction logic for Question2.xaml
+    /// </summary>
+    public partial class Question2 : Window
+    {
+        IList<Student> studentList = new List<Student>()
+        {
+            new Student(1, "John", "Doe", true),
+            new Student(2, "Jim", "Doe", false),
+            new Student(3, "Joe", "Doe", false),
+            new Student(4, "Jan", "Doe", true)
+        };
+        public Question2()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sList.ItemsSource = studentList;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Net_Quiz3
+{
+    class Student
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Boolean isRegistered { get; set; }
+
+        public Student(int id, string fName, string lName, Boolean isReg)
+        {
+            this.Id = id;
+            this.FirstName = fName;
+            this.LastName = lName;
+            this.isRegistered = isReg;
+        }
+    }
+}
